86e6878116ebde1e62d023edf4762922be667513
spyros <morfeas3000@gmail.com>
2020-10-18 17:46:22 +0100

    changes

--- scripts/cre_sync/spreadsheet_to_yaml.py
+++ scripts/cre_sync/spreadsheet_to_yaml.py
@@ -31,6 +31,7 @@ import tempfile
 import jsonschema
 import json
 import os.path
+import logging
 from git import Repo, Git
 from pprint import pprint
 from datetime import datetime
@@ -49,7 +50,8 @@ CRE_LINK_schema = {
         "properties": {
             "CRE-ID-lookup-from-taxonomy-table": {"type": "string"},
             "CS": {"type": "string"},
-            "CWE": {"type": ["number","string"]}, # type string handles the edge-case of empty cell
+            # type string handles the edge-case of empty cell
+            "CWE": {"type": ["number", "string"]},
             "Description": {"type": "string"},
             "Development guide (does not exist for SessionManagement)": {"type": "string"},
             "ID-taxonomy-lookup-from-ASVS-mapping": {"type": "string"},
@@ -58,54 +60,63 @@ CRE_LINK_schema = {
             "OPC": {"type": "string"},
             "Top10 (lookup)": {"type": "string"},
             "WSTG": {"type": "string"},
-        }, 
+        },
         "required": ["CRE-ID-lookup-from-taxonomy-table", "Description"]
     }
-    }
- 
-def readSpreadsheet(url: str,cres_loc:str):
+}
+
+
+logger = logging.getLogger('cre_sync')
+
+
+def readSpreadsheet(url: str, cres_loc: str):
     """given remote google spreadsheet url,
      reads each workbook into a yaml file in the local fs"""
     gc = gspread.oauth()
     sh = gc.open_by_url(url)
+    logger.info("successfully opened spreadsheet %s" % url)
     for wsh in sh.worksheets():
-        print(wsh.title)
         if wsh.title[0].isdigit():
+            logger.info(
+                "handling worksheet %s  (remember, only numbered worksheets will be processed by convention)" % wsh.title)
             records = wsh.get_all_records()
             toyaml = yaml.safe_load(yaml.dump(records))
             try:
                 validateYaml(yamldoc=toyaml, schema=CRE_LINK_schema)
+                logger.info("Worksheet is valid, saving to disk")
+                with open(os.path.join(cres_loc, wsh.title+".yaml"), "wb") as fp:
+                    fp.write(yaml.dump(toyaml, encoding='utf-8'))
             except jsonschema.exceptions.ValidationError as ex:
-                print(wsh.title + " failed validation")
-                print(ex)
-                return
-            with open(os.path.join(cres_loc,wsh.title+".yaml"), "wb") as fp:
-                fp.write(yaml.dump(toyaml,encoding='utf-8'))
-        else:
-            pass
+                logger.error(wsh.title + " failed validation")
+                logger.error(ex)
 
 
-
-def validateYaml(yamldoc: str, schema:str):
+def validateYaml(yamldoc: str, schema: str):
     jsonschema.validate(instance=yamldoc, schema=schema)
 
 
-def pushToGithub(cre_loc:str, apikey, pullRequestName):
-    repo = Repo(os.path.join(os.path.dirname(os.path.realpath(__file__)),"../../"))
+def pushToGithub(cre_loc: str, apikey, pullRequestName):
+    repo = Repo(os.path.join(os.path.dirname(
+        os.path.realpath(__file__)), "../../"))
     # sync first
     # repo.remotes.origin.pu()
     # create branch (cre_sync_timestamp)
     # if remote branch doesn't exist
-    
+
     # all_branches = g.branch("-a")
     # remote_branches = filter(lambda branchName: "remotes" in branchName , g.branch("-a"))
     # if repo.active_branch.name not in remote_branches:
     #     g.branch("--set-upstream-to=origin/%s %s"%(repo.active_branch.name,repo.active_branch.name))
+
     g = Git()
+    commit_msg = "cre_sync_%s" % (datetime.now().isoformat().replace(":","."))
+    logger.info("Pushing new branch %d, make sure you review and merge the branch to master if you want these updates into the REST API")
+    g.checkout("-b",commit_msg)
     g.add(cre_loc)
-    commit_msg = "cre_sync-%s"%datetime.now()
     g.commit("-m",commit_msg)
-    repo.remotes.origin.push()
+    repo.remotes.origin.push("--set-upstream origin %s"%commit_msg)
+    # logger.info("Pushing new branch %d, make sure you review and merge the branch to master if you want these updates into the REST API")
+
     # for r in repo.remotes:
     #     print(r.url)
     # pprint(r)
@@ -126,6 +137,7 @@ def writeSpreadsheet(local, url):
 
 
 spreadsheet_url = "https://docs.google.com/spreadsheets/d/1ACwnuvIwI6Lu3AikNUEeoc4wKoYd9wZ1eL4EtBP50kk/edit#gid=1618195737"
-cre_loc = os.path.join(os.path.dirname(os.path.realpath(__file__)),"../../cres")
+cre_loc = os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), "../../cres")
 readSpreadsheet(spreadsheet_url, cres_loc=cre_loc)
-pushToGithub(cre_loc,"","")
+pushToGithub(cre_loc, "", "")
