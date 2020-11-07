5240b1a533b39076d77445b5afd1bdcaa34799ee
northdpole <morfeas3000@gmail.com>
2020-11-07 17:48:29 +0000

    improvements

--- scripts/cre_sync/spreadsheet_to_yaml.py
+++ scripts/cre_sync/spreadsheet_to_yaml.py
@@ -9,7 +9,6 @@
 #   git merge
 #   push to merge branch
 
-
 # Qs:
 # * do i have fs access with a spreadsheet app?
 # * can i run git commands?
@@ -32,7 +31,8 @@ import jsonschema
 import json
 import os.path
 import logging
-from git import Repo, Git
+import git
+
 from pprint import pprint
 from datetime import datetime
 from github import Github
@@ -43,6 +43,12 @@ from github import Github
 # migrate gspread to work as a bot as well as oauth
 # sync to spreadsheet (different script runs from master)
 # make github action    https://www.jeffgeerling.com/blog/2020/running-github-actions-workflow-on-schedule-and-other-events
+
+spreadsheets_file = "working_spreadsheets.yaml"
+commit_msg_base = "cre_sync_%s" % (datetime.now().isoformat().replace(":", "."))
+
+# gspread_creds_file = " ~/.config/gspread/credentials.json" # OAUTH default credentials location
+# gspread_creds_file = "~/.config/gspread/service_account.json" # Service Account default creds location
 
 CRE_LINK_schema = {
     "type": "array",
@@ -70,60 +76,94 @@ logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+  
 def readSpreadsheet(url: str, cres_loc: str):
     """given remote google spreadsheet url,
-     reads each workbook into a yaml file in the local fs"""
-    gc = gspread.oauth()
-    sh = gc.open_by_url(url)
-    logger.info("successfully opened spreadsheet %s" % url)
-    for wsh in sh.worksheets():
-        if wsh.title[0].isdigit():
-            logger.info(
-                "handling worksheet %s  (remember, only numbered worksheets will be processed by convention)" % wsh.title)
-            records = wsh.get_all_records()
-            toyaml = yaml.safe_load(yaml.dump(records))
-            try:
-                validateYaml(yamldoc=toyaml, schema=CRE_LINK_schema)
-                logger.info("Worksheet is valid, saving to disk")
-                with open(os.path.join(cres_loc, wsh.title+".yaml"), "wb") as fp:
-                    fp.write(yaml.dump(toyaml, encoding='utf-8'))
-            except jsonschema.exceptions.ValidationError as ex:
-                logger.error(wsh.title + " failed validation")
-                logger.error(ex)
+     reads each workbook into a yaml file"""
+    # gc = gspread.oauth() # oauth config, TODO (northdpole): make this configurable
+    try:
+        gc = gspread.service_account()
+        sh = gc.open_by_url(url)
+        logger.info("successfully opened spreadsheet %s" % url)
+        for wsh in sh.worksheets():
+            if wsh.title[0].isdigit():
+                logger.info(
+                    "handling worksheet %s  (remember, only numbered worksheets will be processed by convention)" % wsh.title)
+                records = wsh.get_all_records()
+                toyaml = yaml.safe_load(yaml.dump(records))
+                try:
+                    validateYaml(yamldoc=toyaml, schema=CRE_LINK_schema)
+                    logger.info("Worksheet is valid, saving to disk")
+                    with open(os.path.join(cres_loc, wsh.title + ".yaml"), "wb") as fp:
+                        fp.write(yaml.dump(toyaml, encoding='utf-8'))
+                except jsonschema.exceptions.ValidationError as ex:
+                    logger.error(wsh.title + " failed validation")
+                    logger.error(ex)
+    except gspread.exceptions.APIError as ae:
+        logger.error("Error optining spreadsheet " + url)
+        logger.error(ae)
 
 
 def validateYaml(yamldoc: str, schema: str):
     jsonschema.validate(instance=yamldoc, schema=schema)
 
 
-def pushToGithub(cre_loc: str, apikey:str):
-    repo = Repo(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../"))
-    g = Git()
-    commit_msg = "cre_sync_%s" % (datetime.now().isoformat().replace(":","."))
-    logger.info("Pushing new branch %s, make sure you review and merge the branch to master if you want these updates into the REST API"%commit_msg)
+def create_branch(branch_name):
+    g = git.Git()
+    repo = git.Repo(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../"))
     current_branch = repo.active_branch.name
-    g.checkout("-b",commit_msg)
-    g.add(cre_loc)
-    g.commit("-m",commit_msg)
-    repo.remotes.origin.push(commit_msg)
-    remoteURL = [url for url in repo.remotes.origin.urls]        
-    createPullRequest(apiToken=apikey, repo=remoteURL[0].replace("git@github.com:","").replace(".git",""),
-                      title=commit_msg, srcBranch=commit_msg, targetBranch="master")
+    g.checkout("-b", branch_name)
     g.checkout(current_branch)
 
 
-def createPullRequest(apiToken:str, repo:str, title:str, srcBranch:str, targetBranch:str = "master"):
-    logger.info("Issuing pull request from %s to master for repo %s"%(srcBranch,repo))
+def add_to_tree(cre_loc:str, alias:str):
+    global commit_msg_base
+    commit_msg = "%s-%s"%(commit_msg_base,alias)
+    repo = git.Repo(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../"))
+    g = git.Git()
+    
+    logger.info("Pushing new branch %s, make sure you review and merge the branch to master if you want these updates into the REST API" % commit_msg)
+    current_branch = repo.active_branch.name
+    try:
+        g.checkout(commit_msg)
+        g.add(cre_loc)
+        g.commit("-m", commit_msg)
+    except git.exc.GitCommandError as gce:
+        logger.error("Git error trying to sync " + commit_msg)
+        logger.error(gce)
+    g.checkout(current_branch)
+
+
+def pushToGithub(cre_loc: str, apikey: str):
+    g = git.Git()
+    repo = git.Repo(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../"))
+    g.checkout(commit_msg_base)    
+    repo.remotes.origin.push(commit_msg_base)
+    remoteURL = [url for url in repo.remotes.origin.urls]        
+    createPullRequest(apiToken=apikey, repo=remoteURL[0].replace("git@github.com:", "").replace(".git", ""),
+                      title=commit_msg_base, srcBranch=commit_msg_base, targetBranch="master")
+
+
+def createPullRequest(apiToken:str, repo:str, title:str, srcBranch:str, targetBranch:str="master"):
+    logger.info("Issuing pull request from %s to master for repo %s" % (srcBranch, repo))
     github = Github(apiToken)
-    body = "CRE Sync %s"%title
+    body = "CRE Sync %s" % title
     pr = github.get_repo(repo).create_pull(title=title, body=body, head=srcBranch, base="master")
+
 
 def writeSpreadsheet(local, url):
     pass
 
+def main():
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    cre_loc = os.path.join(script_path, "../../cres")
+    with open(os.path.join(script_path, spreadsheets_file)) as sfile:
+        create_branch(commit_msg_base)
+        urls = yaml.safe_load(sfile)
+        for spreadsheet_url in urls:
+            readSpreadsheet(spreadsheet_url['url'], cres_loc=cre_loc)
+            add_to_tree(cre_loc, spreadsheet_url['alias'])
+        pushToGithub(cre_loc, os.getenv("GITHUB_API_KEY"))
 
-spreadsheet_url = "https://docs.google.com/spreadsheets/d/1ACwnuvIwI6Lu3AikNUEeoc4wKoYd9wZ1eL4EtBP50kk/edit#gid=1618195737"
-cre_loc = os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), "../../cres")
-readSpreadsheet(spreadsheet_url, cres_loc=cre_loc)
-pushToGithub(cre_loc, os.getenv("GITHUB_API_KEY"))
+if __name__ == "__main__":
+    main()
